df5c11c21bc64f7931e52ee2a7a127ac85e30f85
Trugamr <6trugamr9@gmail.com>
2020-06-17 02:23:05 +0530

    updated score keeper layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(QuizzlerApp());
@@ -7,6 +8,11 @@ void main() {
 class QuizzlerApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.grey.shade900,
@@ -24,6 +30,8 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<Icon> scoreKeeper = [];
+
   RaisedButton buildButton({String text, Color color, VoidCallback onPressed}) {
     return RaisedButton(
       onPressed: onPressed,
@@ -39,48 +47,79 @@ class _QuizPageState extends State<QuizPage> {
   @override
   Widget build(BuildContext context) {
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      mainAxisSize: MainAxisSize.max,
       children: <Widget>[
         Expanded(
-          flex: 5,
-          child: Center(
-            child: Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Text(
-                'This is where the question will go. You need to anwer it as true of false.',
-                style: TextStyle(
-                  fontSize: 22.0,
-                  color: Colors.white,
+          child: Column(
+            children: <Widget>[
+              Expanded(
+                flex: 5,
+                child: Center(
+                  child: Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: Text(
+                      'This is where the question will go. You need to anwer it as true of false.',
+                      style: TextStyle(
+                        fontSize: 22.0,
+                        color: Colors.white,
+                      ),
+                      softWrap: true,
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
                 ),
-                softWrap: true,
-                textAlign: TextAlign.center,
               ),
-            ),
+              Expanded(
+                child: Padding(
+                  padding: EdgeInsets.only(
+                      top: 20.0, bottom: 8.0, left: 20.0, right: 20.0),
+                  child: buildButton(
+                    onPressed: () => setState(
+                      () => scoreKeeper.add(
+                        Icon(
+                          Icons.check,
+                          color: Colors.green,
+                        ),
+                      ),
+                    ),
+                    text: 'True',
+                    color: Colors.green,
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: EdgeInsets.only(
+                      top: 8.0, bottom: 20.0, left: 20.0, right: 20.0),
+                  child: buildButton(
+                    onPressed: () => setState(
+                      () => scoreKeeper.add(
+                        Icon(
+                          Icons.close,
+                          color: Colors.red,
+                        ),
+                      ),
+                    ),
+                    text: 'False',
+                    color: Colors.red,
+                  ),
+                ),
+              ),
+            ],
+            crossAxisAlignment: CrossAxisAlignment.stretch,
           ),
         ),
-        Expanded(
-          child: Padding(
-            padding: EdgeInsets.only(
-                top: 20.0, bottom: 8.0, left: 20.0, right: 20.0),
-            child: buildButton(
-              onPressed: () => print('true'),
-              text: 'True',
-              color: Colors.green,
+        Container(
+          height: 40.0,
+          padding: EdgeInsets.only(bottom: 16.0, left: 20.0, right: 20.0),
+          child: Expanded(
+            child: Row(
+              children: scoreKeeper,
             ),
           ),
-        ),
-        Expanded(
-          child: Padding(
-            padding: EdgeInsets.only(
-                top: 8.0, bottom: 20.0, left: 20.0, right: 20.0),
-            child: buildButton(
-              onPressed: () => print('false'),
-              text: 'False',
-              color: Colors.red,
-            ),
-          ),
-        ),
+        )
       ],
-      crossAxisAlignment: CrossAxisAlignment.stretch,
     );
   }
 }
